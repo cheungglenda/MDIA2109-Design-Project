47f7968713762a1444aa9805f6bf5520aa911635
Glenda <gcheung34@my.bcit.ca>
2023-03-04 15:48:02 -0800

    cloned git repo on to my desktop and had to install npm install again

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fc2f8f8b40548797"; }
+/******/ 		__webpack_require__.h = function() { return "93a4fb2d27eda003"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
