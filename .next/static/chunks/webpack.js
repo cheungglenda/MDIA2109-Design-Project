097f8edbcdc053980ab486a0046895a4fe0e5a73
Glenda <gcheung34@my.bcit.ca>
2023-03-04 16:03:42 -0800

    push from my branch

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "758830e26884b990"; }
+/******/ 		__webpack_require__.h = function() { return "31696d1994214f7b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
