71dd73ec3540acc9ddf55f2f7bdcb152372cedc0
anikagrewal <a.grewal.work@gmail.com>
2023-04-17 09:20:59 -0700

    popup to headernav and linked logout to index page

--- components/HeaderNav/index.js
+++ components/HeaderNav/index.js
@@ -2,7 +2,7 @@ import styles from './HeaderNav.module.css'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-
+import HamburgerPopup from '../HamburgerPopup'
 
 export default function HeaderNav({
     headers,
@@ -14,7 +14,9 @@ export default function HeaderNav({
         <div className={styles.headerContainer}>
             <a onClick={() => router.back()} ><Image src="/navIcons/BackButton.svg" alt="placeholder" width={25} height={20} className={styles.backIcon}/></a>
             <h2 className={styles.title}>{headers}</h2>
-            <Link href="/"><Image src="/navIcons/Hamburger.svg" alt="placeholder" width={30} height={20} className={styles.backIcon} /></Link>
+            <div className={styles.hamburger}>
+               <HamburgerPopup />
+               </div>
         </div>
     )
 }
