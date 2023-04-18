5a6d28bde8ddb6214cf980ad099ed89716d34418
Glenda <gcheung34@my.bcit.ca>
2023-04-18 11:46:27 -0700

    free commit

--- data/quiz/quiz.js
+++ data/quiz/quiz.js
@@ -4,36 +4,36 @@ export const quizData = {
         question: "What are some essential emergency supplies to keep in your car?",
         options: ["Spare tire a tire iron and a jack, Jumper Cables and a portable battery charger, A first-aid kit and emergency blanket, All of the above "],
         answer: "All of the above",
-        imageCorrect: "/quizImages/rightAnswer.svg",
-        imageWrong: "/quizImages/wrongAnswer.svg"
+        imageCorrect: "/quizImages/rightAnswer.png",
+        imageWrong: "/quizImages/wrongAnswer.png"
     },
     {
         question: "Why is it important to keep emergency supplies in your car?",
         options: ["To prepare for unexpected emergencies, To make sure you are comfortable during a long road trip, To impress your friends with your preparedness skills, To save money on emergency supplies in case of a crisis"],
         answer: "To prepare for unexpected emergencies",
-        imageCorrect: "/quizImages/rightAnswer.svg",
-        imageWrong: "/quizImages/wrongAnswer.svg"
+        imageCorrect: "/quizImages/rightAnswer.png",
+        imageWrong: "/quizImages/wrongAnswer.png"
     },
     {
         question: "What are some tips for staying calm during a crisis like being stuck in traffic for four hours?",
         options: ["To take deep breaths and try to relax your body, Listen to calming music or a podcast, Play a game or read a book to distract yoursel, All of the above"],
         answer: "All of the above",
-        imageCorrect: "/quizImages/rightAnswer.svg",
-        imageWrong: "/quizImages/wrongAnswer.svg"
+        imageCorrect: "/quizImages/rightAnswer.png",
+        imageWrong: "/quizImages/wrongAnswer.png"
     },
     {
         question: "What should you do if you become stranded in your car during a winter storm?",
         options: ["Stay in your car and wait for help to arrive, Turn off the engine to conserve fuel and prevent carbon monoxide posioning, Keep warm by wrapping yourself in blankets or other warm clothing, All of the above"],
         answer: "All of the above",
-        imageCorrect: "/quizImages/rightAnswer.svg",
-        imageWrong: "/quizImages/wrongAnswer.svg"
+        imageCorrect: "/quizImages/rightAnswer.png",
+        imageWrong: "/quizImages/wrongAnswer.png"
     },
     {
         question: "Why is it important to have a plan for emergencies when travelling by car?",
         options: ["To ensure your saftey and safety to your passengers, To impress your friends with your preparedness skills, To avoid getting lost or running out of gas, None of the above"],
         answer: "To ensure your safety and safety to your passengers",
-        imageCorrect: "/quizImages/rightAnswer.svg",
-        imageWrong: "/quizImages/wrongAnswer.svg"
+        imageCorrect: "/quizImages/rightAnswer.png",
+        imageWrong: "/quizImages/wrongAnswer.png"
     },
 ]
 }
